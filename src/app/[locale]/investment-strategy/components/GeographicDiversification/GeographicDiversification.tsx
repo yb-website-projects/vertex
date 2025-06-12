03c82bbc1a72bcb48d86f0722cf0c0a5efa48bb6
SergWebAcceptance <serg.web@acceptance.ltd>
2025-06-12 12:55:52 +0300

    Enhanced styles and layout for GeographicDiversification component, improving responsiveness and visual consistency. Updated iframe borders and adjusted flex properties for better alignment on tablet screens.

--- src/app/[locale]/investment-strategy/components/GeographicDiversification/GeographicDiversification.tsx
+++ src/app/[locale]/investment-strategy/components/GeographicDiversification/GeographicDiversification.tsx
@@ -87,7 +87,6 @@ export const GeographicDiversification = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255278.11535313536!2d103.6592300280363!3d1.3440836619253285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1767b42b8ec9%3A0x400f7acaedaa420!2sSingapore!5e0!3m2!1sen!2sua!4v1749721491231!5m2!1sen!2sua"
                   width="600"
                   height="450"
-                  style={{ border: '0' }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -98,7 +97,6 @@ export const GeographicDiversification = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3487844.078128611!2d32.44044890242836!3d31.383868601420126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1500492432a7c98b%3A0x6a6b422013352cba!2sIsrael!5e0!3m2!1sen!2sua!4v1749721588661!5m2!1sen!2sua"
                   width="600"
                   height="450"
-                  style={{ border: '0' }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -109,7 +107,6 @@ export const GeographicDiversification = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15283138.46067924!2d6.628572696101025!3d48.602539317964094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ed8886cfadda85%3A0x72ef99e6b3fcf079!2sEurope!5e0!3m2!1sen!2sua!4v1749721617528!5m2!1sen!2sua"
                   width="600"
                   height="450"
-                  style={{ border: '0' }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -120,7 +117,6 @@ export const GeographicDiversification = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26655348.5134428!2d-125.56361818557603!3d46.70314525487429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b30b71698e729d%3A0x131328839761a382!2sNorth%20America!5e0!3m2!1sen!2sua!4v1749721641246!5m2!1sen!2sua"
                   width="600"
                   height="450"
-                  style={{ border: '0' }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
@@ -131,7 +127,6 @@ export const GeographicDiversification = () => {
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722544.305707933!2d51.30668040481522!3d24.336138810224497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e48dfb1ab12bd%3A0x33d32f56c0080aa7!2sUnited%20Arab%20Emirates!5e0!3m2!1sen!2sua!4v1749721658230!5m2!1sen!2sua"
                   width="600"
                   height="450"
-                  style={{ border: '0' }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
